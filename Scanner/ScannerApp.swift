481b86138dfab53dab9a739796e31a6dd53bae2b
githubuiux <uiuxista@gmail.com>
2023-07-04 12:44:52 +0300

    Initial Commit

---
+++ Scanner/ScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerApp.swift
+//  Scanner
+//
+//  Created by Levent on 4.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
